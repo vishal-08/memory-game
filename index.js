4e0cf584f60ee564278dd089bcba4f1cc319b6d1
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-06 18:07:39 +0530

    game Finished

--- index.js
+++ index.js
@@ -1,15 +1,102 @@
 const cards = document.querySelectorAll(".grid-item")
+const scoreEl = document.getElementById("score")
+const failedEl = document.getElementById("failed-el")
+let score = 0
+let failed = 0
+//first card is not flipped
+let isFirstCardFlipped = false;
+let freezeCards = false
+let firstCard
+let secondCard
+
 
 function flipCard(){
-    
-    
-    this.classList.toggle("flip")
-    setTimeout(() => {
-        this.classList.remove('flip');
-        
-      }, 1500);
+    if(freezeCards) return
+    if(firstCard === this) return
 
+    this.classList.add("flip")
+   
+    if(!isFirstCardFlipped)
+    {
+      firstCard = this
+      isFirstCardFlipped = true
+
+      return
+    }
+    
+    secondCard = this
+    
+      
+    cardMatches()
 }
+
+function cardMatches(){
+  if(firstCard.dataset.avengers === secondCard.dataset.avengers)
+    {
+      disableCards()
+      score += 10
+      scoreEl.innerHTML = `Score : ${score}`
+      
+    }
+    else
+    {
+      reFlipCard()
+      failed += 1
+      if(failed<10)
+      {
+        failedEl.innerHTML = `Failed Attempt : 0${failed}`
+      }
+      else
+      {
+        failedEl.innerHTML = `Failed Attempt : ${failed}`
+      }
+      
+    }
+    
+}
+
+function reFlipCard()
+{
+
+  freezeCards = true
+  setTimeout(() => {
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
+
+    resetCards()
+  },1500)
+  
+ 
+}
+
+
+function disableCards()
+{
+  firstCard.removeEventListener("click", flipCard)
+  secondCard.removeEventListener("click", flipCard)
+  resetCards()
+  
+}
+
+function shuffle() {
+  cards.forEach(card => {
+    let suffleCards = Math.floor(Math.random() * 12)+1;
+    card.style.order = suffleCards;
+  });
+}
+
+
+function resetCards(){
+  isFirstCardFlipped = false
+  freezeCards = false
+  firstCard = null
+  secondCard = null
+}
+
+shuffle()
+
+
+
 
 cards.forEach(card => card.addEventListener("click", flipCard))
 
